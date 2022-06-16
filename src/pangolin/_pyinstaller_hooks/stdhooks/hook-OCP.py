41c4679bf38e0df39312ad6f26c2ac200848de6b
Olle Vidner <olle@vidner.se>
2022-06-16 18:07:21 +0200

    Stop using conda as source of files
    That method includes the `.nonadmin` file on Windows as a binary,
    resulting in the weird WinError 1006 error when PyInstaller tries to
    do LoadLibraryEx on the `.nonadmin` file.

--- src/pangolin/_pyinstaller_hooks/stdhooks/hook-OCP.py
+++ src/pangolin/_pyinstaller_hooks/stdhooks/hook-OCP.py
@@ -1,11 +1,3 @@
-from PyInstaller.compat import is_pure_conda
+import importlib
 
-if is_pure_conda:
-    from PyInstaller.utils.hooks import conda_support
-
-    binaries = [(f.locate(), ".") for f in conda_support.files("ocp")]
-
-else:
-    import importlib
-
-    binaries = [(importlib.util.find_spec("OCP").origin, ".")]
+binaries = [(importlib.util.find_spec("OCP").origin, ".")]
