c0c72c161debd30727b29b94860edfd690e836a5
Olle Vidner <olle@vidner.se>
2023-08-03 17:14:13 +0200

    Break out dataset meta checking

--- src/facit/testing.py
+++ src/facit/testing.py
@@ -2,14 +2,24 @@ import deepdiff
 from xarray.testing import assert_equal
 
 
-def assert_ds_equal(a, b):
-    def _all_attrs(x):
-        return {name: var.attrs for name, var in x.variables.items()}
+def _ds_variable_attrs(x):
+    return {name: var.attrs for name, var in x.variables.items()}
 
+
+def assert_ds_meta_equal(a, b):
     assert_equal(a, b)
     assert not deepdiff.DeepDiff(a.attrs, b.attrs)
     assert not deepdiff.DeepDiff(
-        _all_attrs(a),
-        _all_attrs(b),
+        _ds_variable_attrs(a),
+        _ds_variable_attrs(b),
     )
     assert a.variables.keys() == b.variables.keys()
+
+
+def assert_ds_equal(a, b):
+    """
+    Assert that two xarray Datasets are equal, including metadata and
+    attributes.
+    """
+    assert_ds_meta_equal(a, b)
+    assert_equal(a, b)
